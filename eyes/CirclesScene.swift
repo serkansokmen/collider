b06038e98414e23f8bce125b09e830bde6a5b4fe
Serkan Sokmen <e.serkan.sokmen@gmail.com>
2016-04-28 03:58:11 +0300

    ibstacles added

--- eyes/CirclesScene.swift
+++ eyes/CirclesScene.swift
@@ -11,6 +11,7 @@ class CirclesScene: SKScene, SKPhysicsContactDelegate {
     var hitSound: AudioPlayer?
     
     let shapes = ["circle", "rectangle", "rounded"]
+    var obstacles = [SKSpriteNode]()
     
     override func didMoveToView(view: SKView) {
         
@@ -23,6 +24,10 @@ class CirclesScene: SKScene, SKPhysicsContactDelegate {
 //        bg.position = CGPointMake(frame.size.width/2, frame.size.height/2);
 //        bg.zPosition = -1
 //        addChild(bg)
+        
+        let mill = createBody(atLocation: frame.origin, withImageName: "mill")
+        mill.physicsBody?.dynamic = false
+        obstacles.append(mill)
         
         motionManager = CMMotionManager()
         motionManager.startAccelerometerUpdates()
@@ -53,18 +58,20 @@ class CirclesScene: SKScene, SKPhysicsContactDelegate {
                 node = createBody(atLocation: touch.locationInNode(self), withImageName: "rounded")
             }
             node.physicsBody?.velocity = dir
-            addChild(node)
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
         
-        theta = theta + 0.001
-        let radius = min(frame.width, frame.height) / 2
-        if let light = childNodeWithName("SceneLight") {
-            light.position.x = CGFloat(radius) * sin(CGFloat(theta))
-            light.position.y = CGFloat(radius) * cos(CGFloat(theta))
-            print(light.position)
+        theta = theta + 0.01
+        let radius = 5.0
+        let s = sin(CGFloat(theta))
+        let c = cos(CGFloat(theta))
+        
+        for obstacle in obstacles {
+            obstacle.position.x = view!.bounds.width / 2 + CGFloat(radius) * s
+            obstacle.position.y = view!.bounds.height / 2 + CGFloat(radius) * c
+            obstacle.zRotation = c
         }
         
         if let accelerometerData = motionManager.accelerometerData {
@@ -82,15 +89,15 @@ class CirclesScene: SKScene, SKPhysicsContactDelegate {
     func createBody(atLocation location: CGPoint, withImageName imageName: String) -> SKSpriteNode {
         
         let rand = CGFloat(RandomFloat(min: 0.2, max: 0.5))
-        let spark = SKSpriteNode(imageNamed: imageName)
-        spark.physicsBody = SKPhysicsBody(texture: SKTexture(imageNamed: imageName), size: spark.size)
+        let sprite = SKSpriteNode(imageNamed: imageName)
+        sprite.physicsBody = SKPhysicsBody(texture: SKTexture(imageNamed: imageName), size: sprite.size)
         
-        spark.name = imageName
-        spark.position = location
-        spark.setScale(CGFloat(rand))
-        spark.zPosition = 1
+        sprite.name = imageName
+        sprite.position = location
+        sprite.setScale(CGFloat(rand))
+        sprite.zPosition = 1
         
-        if let physics = spark.physicsBody {
+        if let physics = sprite.physicsBody {
             physics.affectedByGravity = true
             physics.allowsRotation = true
             physics.dynamic = true
@@ -98,10 +105,11 @@ class CirclesScene: SKScene, SKPhysicsContactDelegate {
             physics.angularVelocity = 0.75
             physics.linearDamping = 0.01
             physics.angularDamping = 0.01
-            physics.contactTestBitMask = spark.physicsBody!.collisionBitMask
+            physics.contactTestBitMask = sprite.physicsBody!.collisionBitMask
         }
+        addChild(sprite)
         
-        return spark
+        return sprite
     }
     
     func didBeginContact(contact: SKPhysicsContact) {
